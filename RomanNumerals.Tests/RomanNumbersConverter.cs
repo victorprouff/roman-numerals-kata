debe1510875419e8282fc8ac3cf8452b1169cfa6
Victor Prouff <contact@victorprouff.fr>
2023-08-31 16:27:33 +0200

    Init class

--- RomanNumerals.Tests/RomanNumbersConverter.cs
+++ RomanNumerals.Tests/RomanNumbersConverter.cs
@@ -1,6 +1,6 @@
 namespace RomanNumerals.Tests;
 
-public class UnitTest1
+public class RomanNumbersConverter
 {
     [Fact]
     public void Test1()
---
+++ RomanNumerals/RomanNumbersConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace RomanNumerals;
+
+public static class RomanNumbersConverter
+{
+    public static string ArabicToRomanNumeral(this int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static string RomanNumeralToArabic(this string number)
+    {
+        throw new NotImplementedException();
+    }
+
+}
