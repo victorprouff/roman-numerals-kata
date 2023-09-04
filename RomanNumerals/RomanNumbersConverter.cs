781bc66df7945631bb68c7bb7d7f5eeebaaf2b4f
Victor Prouff <contact@victorprouff.fr>
2023-09-04 11:44:34 +0200

    Refactoring : compact ArabicToRomanNumeral method

--- RomanNumerals/RomanNumbersConverter.cs
+++ RomanNumerals/RomanNumbersConverter.cs
@@ -4,17 +4,13 @@ public static class RomanNumbersConverter
 {
     public static string ArabicToRomanNumeral(this int number)
     {
-        var result = string.Empty;
-
         var ones = new OnesNumber(number);
         var tens = new TensNumber(number);
         var hundreds = new HundredNumber(number);
 
-        result += hundreds.CalculateFromArabicNumber();
-        result += tens.CalculateFromArabicNumber();
-        result += ones.CalculateFromArabicNumber();
-
-        return result;
+        return hundreds.CalculateFromArabicNumber()
+               + tens.CalculateFromArabicNumber()
+               + ones.CalculateFromArabicNumber();
     }
 
     public static string RomanNumeralToArabic(this string number)
